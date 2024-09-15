4955ca1e71de178aaeb6647c34711ad8c8531d4b
aaronjmill <97998063+aaronjmill@users.noreply.github.com>
2024-09-15 15:26:03 -0400

    Update z80_code.s

--- src/z80/z80_code.s
+++ src/z80/z80_code.s
@@ -1,1 +1,137 @@
+; z80_code.s - Z80 sound processing and communication for Sega Saturn
+; Ported from Batman Returns (68K Assembly) to SH-2 Assembly
+; Date: [Insert Date]
+; Author: [Your Name]
+
+    .section .text
+    .global _z80_start
+
+; ==========================================================================
+; Z80 Initialization Section
+; ==========================================================================
+
+_z80_start:
+    ; Initialize the Z80 sound processor
+    MOV.W   #$100, Z80Reset           ; Reset the Z80
+    MOV.W   #$100, Z80BusReq          ; Request bus control
+
+    ; Wait for bus grant
+    btst.b  #0, Z80BusReq
+    bne     .wait_bus_grant
+
+    ; Copy sound data to Z80 RAM
+    LEA     Z80_0, A0                 ; Source address (68K to Z80 space)
+    MOV.L   Z80CLEN, D0               ; Length of data to move
+    LEA     Z80Ram, A1                ; Z80 RAM destination
+.lzlp1:
+    MOV.B   (A0)+, (A1)+
+    DBRA    D0, .lzlp1
+
+    ; Zero-fill remaining 8K block
+    MOV.B   #0, (A1)+
+    CMPA.L  #(Z80Ram+$2000), A1
+    BNE.S   .lzlp1
+
+    ; Start the Z80 processor
+    MOV.W   #$0, Z80Reset             ; Release Z80 reset
+    MOV.L   #15, D0                   ; Delay for > 26 µS
+.szlp:
+    SUBQ.L  #1, D0
+    BNE.S   .szlp
+    MOV.W   #$0, Z80BusReq            ; Release bus request
+
+    RTS
+
+; ==========================================================================
+; Sound Processing Main Loop
+; ==========================================================================
+
+AUDIO_VBL_MAIN:
+    MOVEM.L A0-A2/D0-D2, -(SP)        ; Save registers
+
+    ; Main sound buffer handling and communication
+    LEA     MAIN_SFX_XBUFF, A0        ; Load buffer address
+    MOVE.L  A0, BUFFADD               ; Set buffer address
+
+    ; Check and process sound effects
+    tst.w   MSFX_FLAG
+    beq     .z80_00                   ; If no SFX, skip to Z80 communication
+
+    ; Handle SFX and MIDI/PCM updates
+    LEA     MMC_BLOKS, A1
+    tst.l   MMC_PNTR(A1)
+    beq     .z80_01
+.z80_08:
+    SUB.W   #1, MMC_ticks(A1)
+    bpl     .z80_01
+
+    ; Update MIDI/PCM pointers and data
+    MOV.L   MMC_PNTR(A1), A2
+    ADDQ    #1, A2
+    MOV.B   (A2)+, D0
+    CMP.B   #PRGM, D0
+    BNE     .z80_03
+
+    ; Handle MIDI program change
+    MOVE.B  #Z80_CMD, (A0)+
+    MOVE.B  #Z80_PRG, (A0)+
+    MOVE.B  D2, (A0)+
+    ADDQ    #1, A2
+    MOVE.B  (A2)+, (A0)+
+    ADD.W   #4, MAIN_SFX_XBUFF
+    BRA     .z80_09
+
+.z80_03:
+    ; Handle other MIDI/PCM commands (VOLM, NTON, NTOF, etc.)
+    ; ...
+
+.z80_00:
+    ; Handle Z80 communication
+    MOV.L   BUFFADD, A0
+    LEA     Z80Ram+$36, A1
+    LEA     Z80Ram+$1C40, A2
+.z80w0:
+    btst.b  #0, Z80BusReq
+    bne.s   .z80w0
+
+    ; Transfer data to Z80 FIFO
+    MOV.W   (A0)+, D2
+    beq     .z80exit
+    MOV.B   (A1), D1
+.z80loop:
+    MOV.B   (A0)+, D0
+    EXT.W   D1
+    MOV.B   D0, (A2, D1.w)
+    ADDQ.B  #1, D1
+    ANDI.B  #$3F, D1
+    SUBQ.W  #1, D2
+    BNE     .z80loop
+    MOV.B   D1, (A1)
+
+.z80exit:
+    ; Finish up and release Z80 bus
+    MOV.W   #0, Z80BusReq
+    MOVEM.L (SP)+, A0-A2/D0-D2        ; Restore registers
+
+    RTS
+
+; ==========================================================================
+; Z80 Communication and Control Macros
+; ==========================================================================
+
+Z80BYTE MACRO
+    ; Macro to send a byte to the Z80
+    MOV.B   \1, @R5:R5.W              ; Send byte to Z80
+    ADD     #1, R5
+    AND     #Z80_XBSIZE-1, R5
+    ENDM
+
+Z80CHAN MACRO
+    ; Macro to send channel data to Z80
+    MOV.B   Curchan+1, @R5:R5.W       ; Send current channel data
+    ADD     #1, R5
+    AND     #Z80_XBSIZE-1, R5
+    ENDM
+
+; End of z80_code.s
 
